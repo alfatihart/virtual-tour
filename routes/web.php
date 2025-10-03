e81c8793e51fa48375b1ee5e3a9d9cb3d71b9de6
Al-Fatih Art <restu6005@gmail.com>
2025-10-04 02:04:37 +0800

    feat: define routes for login, logout, and admin access

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
+});
+
+Route::get('/login', [AuthController::class, 'index'])->name('login');
+Route::post('/login', [AuthController::class, 'store'])->name('login.store');
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/admin', function () {
+        return view('pages.admin.home.index');
+    });
 });
